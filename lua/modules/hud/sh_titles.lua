fcfe9d43d6da0f8947e6c792e2f2941b57891736
DaDamRival <dadude770@gmail.com>
2017-03-03 17:31:45 +0100

    Fixed title database

--- lua/modules/hud/sh_titles.lua
+++ lua/modules/hud/sh_titles.lua
@@ -1,66 +1,51 @@
 if SERVER then
-	if sql.TableExists("title_data") then
-		sql.Query("CREATE TABLE IF NOT EXISTS title_data ( UniqueID string, Title INTEGER NOT NULL );")
-		--[[print("----------------------------------------------------------------------------------------------------------------")
-		print(sql.LastError())
-		print("----------------------------------------------------------------------------------------------------------------")]]--
+	if not sql.TableExists("dadam_titles") then
+		sql.Query("CREATE TABLE dadam_titles ( steamid64 varchar(255), title varchar(255) )")
+		print("Attempting to create title database")
+		
+		if sql.TableExists("dadam_titles") then
+			print("Title database has been created")
+		end
 	end
 	
 	hook.Add("PlayerSay", "dadam_title", function(ply, text, public)
 		if string.sub(text, 1, 6) == "!title" then
-			arg = string.sub(text, 8, #text)
+			local arg = string.sub(text, 8, #text)
+			local id = ply:SteamID64()
 			
 			if #arg > 0 then
-				ply:ChatPrint("Your title is now: "..string.sub(arg, 1, 32))
+				ply:ChatPrint("Your title is now: " .. string.sub(arg, 1, 32))
 				
 				if #arg > 32 then
 					ply:ChatPrint("Note: Your title hit the char limit of 32 chars and has been shortend.")
 				end
 				
-				--print(ply:Nick().." changed his title to: "..arg.."("..#arg..")")
-				--print(ply:Nick().." changed his title to: "..string.sub(arg, 1, 32))
-				
+				sql.Query("UPDATE dadam_titles SET title = '" .. string.sub(arg, 1, 32) .. "' WHERE steamid64 = '" .. id .. "'")
 				ply:SetNWString("dadam_title", string.sub(arg, 1, 32))
-				
-				--[[local result = sql.Query("SELECT Title FROM title_data WHERE UniqueID = "..ply:UniqueID()..";")
-				print("----------------------------------------------------------------------------------------------------------------")
-				print(result)
-				print("----------------------------------------------------------------------------------------------------------------")
-				if result then
-					sql.Query("UPDATE title_data SET Title = "..string.sub(arg, 1, 32).." WHERE UniqueID = "..ply:UniqueID()..");")
-					ply:ChatPrint("DEBUG: UPDATE; "..sql.LastError())
-				else
-					sql.Query("INSERT into title_data ( UniqueID, Title ) VALUES ( UniqueID = "..ply:UniqueID()..", Title = "..string.sub(arg, 1, 32)..");")
-					ply:ChatPrint("DEBUG: MAKE; "..sql.LastError())
-				end]]--
-				
-				--ply:ChatPrint("DEBUG: "..sql.Query("SELECT Title FROM title_data WHERE UniqueID = '"..ply:UniqueID().."'"))
-				
-				--sql.Query("UPDATE title_data SET Title = '"..arg.."' WHERE UniqueID = '"..ply:UniqueID().."'")
 			else
 				if #ply:GetNWString("dadam_title") > 0 then
-					ply:ChatPrint("Your Title is: "..ply:GetNWString("dadam_title"))
+					ply:ChatPrint("Your Title is: " .. ply:GetNWString("dadam_title"))
 				else
 					ply:ChatPrint("You dont have a title. ")
 				end
 			end
+		elseif string.sub(text, 1, 11) == "!cleartitle" then
+			local id = ply:SteamID64()
+			
+			sql.Query("UPDATE dadam_titles SET title = '' WHERE steamid64 = '" .. id .. "'")
+			ply:SetNWString("dadam_title", "")
 		end
 	end )
 	
 	hook.Add("PlayerInitialSpawn", "dadam_title", function(ply)
-		local result = sql.QueryValue("SELECT Title FROM title_data WHERE UniqueID = "..ply:UniqueID()..";")
+		local id = ply:SteamID64()
+		local result = sql.QueryRow("SELECT title FROM dadam_titles WHERE steamid64 = '" .. id .. "'")
+
 		if result then
-			ply:SetNWString("dadam_title", result)
-		end
-		
-		--[[local result = sql.Query("SELECT Title FROM title_data WHERE UniqueID = "..ply:UniqueID())
-		
-		if !IsValid(result) then
-			sql.Query("INSERT INTO title_data ( UniqueID, OriginalTitle, Title ) VALUES ('" .. ply:UniqueID() .. "','" .. ply:Nick() .. "','')")
-			ply:SetNWString("dadam_title", "")
+			ply:SetNWString("dadam_title", result.title)
 		else
-			ply:SetNWString("dadam_title", result[1]["Title"])
-		end]]--
+			sql.Query("INSERT into dadam_titles (steamid64, title) VALUES ('" .. id .. "', '')")
+		end
 	end)
 else
 	surface.CreateFont("dadam_title_font", {
