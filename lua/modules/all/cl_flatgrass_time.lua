f5c2d382bc99cf4370b3f9186952ffad3bd4e366
DaDamRival <dadude770@gmail.com>
2017-03-03 15:47:21 +0100

    Fixed flatgrass scripts not working on spawn player

--- lua/modules/all/cl_flatgrass_time.lua
+++ lua/modules/all/cl_flatgrass_time.lua
@@ -1,13 +1,15 @@
-if  game.getMap() == "gm_flatgrass" then
-	-- Remove Flattywood and small city in background
-	Material("gm_construct/flatsign"):SetInt("$alpha", 0)
-	Material("gm_construct/citycard01"):SetInt("$alpha", 0)
+hook.Add("ULibLocalPlayerReady", "dadam_flatgrasstime", function(plyrdy)
+	if game.GetMap() == "gm_flatgrass" then
+		-- Remove Flattywood and small city in background
+		Material("gm_construct/flatsign"):SetInt("$alpha", 0)
+		Material("gm_construct/citycard01"):SetInt("$alpha", 0)
 
-	hook.Add("PostDrawOpaqueRenderables", "3DTimeDisplay", function()
-		cam.Start3D2D(Vector(-5058.207031, 0.759911, -15310.609375), Angle(0, 95, 70), 5)
+		hook.Add("PostDrawOpaqueRenderables", "3DTimeDisplay", function()
+			cam.Start3D2D(Vector(-5058.207031, 0.759911, -15310.609375), Angle(0, 95, 70), 5)
 
-		draw.SimpleText(os.date("%H:%M:%S", os.time()), "DermaLarge", 10, 7)
+			draw.SimpleText(os.date("%H:%M:%S", os.time()), "DermaLarge", 10, 7)
 
-		cam.End3D2D()
-	end)
-end
+			cam.End3D2D()
+		end)
+	end
+end)
