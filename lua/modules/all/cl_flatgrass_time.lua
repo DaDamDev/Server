176e40c58d58528886e6ba519eba9bda4da14bb1
its-suun <suun@teknik.io>
2017-03-04 15:34:29 +0100

    Fix 'dadam_flatgrastime' typo

--- lua/modules/all/cl_flatgrass_time.lua
+++ lua/modules/all/cl_flatgrass_time.lua
@@ -5,7 +5,7 @@ hook.Add("ULibLocalPlayerReady", "dadam_flatgrasstime", function(plyrdy)
 			Material("gm_construct/flatsign"):SetInt("$alpha", 0)
 			Material("gm_construct/citycard01"):SetInt("$alpha", 0)
 
-			hook.Add("PostDrawOpaqueRenderables", "dadam_flatgrastime", function()
+			hook.Add("PostDrawOpaqueRenderables", "dadam_flatgrasstime", function()
 				cam.Start3D2D(Vector(-5058.207031, 0.759911, -15310.609375), Angle(0, 95, 70), 5)
 
 				draw.SimpleText(os.date("%H:%M:%S", os.time()), "DermaLarge", 10, 7)
