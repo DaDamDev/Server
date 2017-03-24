570b524daf80ea3dd2494e7a9d5793edc92cd1af
DaDamRival <dadude770@gmail.com>
2017-03-24 16:09:20 +0100

    Added basic settings menu

---
+++ lua/modules/ui/cl_settingsmenu.lua
@@ -0,0 +1,57 @@
+local base
+
+local function open()
+	if not base then
+		local w, h = ScrW(), ScrH()
+		
+		base = vgui.Create("DFrame")
+		base:SetPos(w/2 - 150, h/2 - 250)
+		base:SetSize(300, 500)
+		base:SetTitle("DaDamDev Settings")
+		base:MakePopup()
+		base:SetDeleteOnClose(false)
+		function base:OnClose() base:Hide() end
+		
+		-----Skin-----
+		local skin = vgui.Create("DCollapsibleCategory", base)
+		skin:SetPos(25, 50)
+		skin:SetSize(150, 100)
+		skin:SetExpanded(0)
+		skin:SetLabel("UI Skin (" .. dadam.skin .. ")")
+		
+		local skinList = vgui.Create("DPanelList", aimbotFrame)
+		skinList:EnableHorizontal(false)
+		skinList:EnableVerticalScrollbar(true)
+		skin:SetContents(skinList)
+		
+		local skinWhite = vgui.Create("DButton")
+		skinWhite:SetText("White")
+		skinList:AddItem(skinWhite)
+		function skinWhite:DoClick() dadam.skin = "default" dadam.saveSettings() skin:SetLabel("UI Skin (" .. dadam.skin .. ")") end
+		
+		local skinDark = vgui.Create("DButton")
+		skinDark:SetText("Dark")
+		skinList:AddItem(skinDark)
+		function skinDark:DoClick() dadam.skin = "CISKIN" dadam.saveSettings()  skin:SetLabel("UI Skin (" .. dadam.skin .. ")") end
+		
+		local skinSpacer = vgui.Create("DLabel", parent)
+		skinSpacer:SetHeight(10)
+		skinSpacer:SetText("")
+		skinList:AddItem(skinSpacer)
+		
+		local skinNote = vgui.Create("DButton", parent)
+		skinNote:SetHeight(30)
+		skinNote:SetText("Requires rejoin to work!\nClick here to rejoin the server.")
+		skinList:AddItem(skinNote)
+		function skinNote:DoClick() LocalPlayer():ConCommand("retry") end
+		--------------
+	else
+		base:Show()
+	end
+end
+
+hook.Add("OnPlayerChat", "dadam_settings", function(ply, txt, team, ded)
+	if ply == LocalPlayer() and txt == "!settings" then
+		open()
+	end
+end)
