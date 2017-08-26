8fec961cb085ceb3737a6df14ffd634cb9cc7976
its-suun <suun@teknik.io>
2017-08-26 18:57:43 +0200

    Add loading screen

--- lua/modules/all/sh_antiskybox.lua
+++
@@ -1,9 +0,0 @@
-local map = game.GetMap()
-
-hook.Add("FinishMove", "dadam_antiskybox", function(ply, mvd)
-    local pos = mvd:GetOrigin()
-    if pos.z < -12826 and map == "gm_flatgrass" then
-        ply:SetPos(Vector(pos.x, pos.y, -12826))
-        return true
-    end
-end)
