ba086f397c4c4ee44e25c1160396045bf491c2aa
suun <suun@teknik.io>
2018-07-15 21:04:58 +0200

    Create sv_spawn.lua

---
+++ lua/modules/map/sv_spawn.lua
@@ -0,0 +1,53 @@
+if game.GetMap() ~= "gm_cloudbuild" then return end
+
+local spawnpoints = {
+	Vector(8528, -192, 608),
+	Vector(8528, -128, 608),
+	Vector(8528, -64, 608),
+	Vector(8464, -64, 608),
+	Vector(8464, -128, 608),
+	Vector(8464, -192, 608),
+	Vector(8400, -192, 608),
+	Vector(8400, -160, 608),
+	Vector(8400, -128, 608),
+	Vector(8400, -64, 608),
+	Vector(8336, -64, 608),
+	Vector(8336, -128, 608),
+	Vector(8336, -192, 608),
+	Vector(8272, -192, 608),
+	Vector(8272, -128, 608),
+	Vector(8272, -64, 608),
+	Vector(8208, -64, 608),
+	Vector(8208, -128, 608),
+	Vector(8208, -192, 608),
+	Vector(8144, -192, 608),
+	Vector(8144, -128, 608),
+	Vector(8144, -64, 608),
+	Vector(8152, 0, 608),
+	Vector(8208, 0, 608),
+	Vector(8272, 0, 608),
+	Vector(8336, 0, 608),
+	Vector(8400, 0, 608),
+	Vector(8464, 0, 608),
+	Vector(8528, 0, 608),
+	Vector(8528, 64, 608),
+	Vector(8464, 64, 608),
+	Vector(8400, 64, 608),
+	Vector(8336, 64, 608),
+	Vector(8272, 64, 608),
+	Vector(8208, 64, 608),
+	Vector(8144, 64, 608),
+	Vector(8592, 64, 608),
+	Vector(8592, 0, 608),
+	Vector(8592, -64, 608),
+	Vector(8592, -128, 608),
+	Vector(8592, -192, 608),
+}
+
+
+
+hook.Add("PlayerSpawn", "sbv_MapSpawn", function(ply)
+	if not ply.customspawn then
+		ply:SetPos(spawnpoints[math.random(#spawnpoints)])
+	end
+end)
